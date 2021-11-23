9504e2a8806c59bd61557ac5a849f1a12e0a4dbb
Omar <omared250@gmail.com>
2021-11-23 15:20:35 +0100

    decresing the score

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /* console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number!';
@@ -12,6 +11,8 @@ document.querySelector('.guess').value = 23; */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
@@ -20,5 +21,13 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.message').textContent = 'No number!';
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!'
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too high!'
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'Too low!'
+        score--;
+        document.querySelector('.score').textContent = score;
     }
 });
