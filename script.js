a7b4765b5cfa04609c7d00ac1b252b2a513b968c
Omar <omared250@gmail.com>
2021-11-23 15:48:17 +0100

    adding green background to winners

--- script.js
+++ script.js
@@ -17,17 +17,37 @@ document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+    // When there is not input
     if (!guess) {
         document.querySelector('.message').textContent = 'No number!';
+    
+    // When players win
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = 'Correct Number!'
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+    // When guess is too high
     } else if (guess > secretNumber) {
-        document.querySelector('.message').textContent = 'Too high!'
-        score--;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;            
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!!'
+            document.querySelector('.score').textContent = 0;
+        }
+    
+    // When guess is to low
     } else if (guess < secretNumber) {
-        document.querySelector('.message').textContent = 'Too low!'
-        score--;
-        document.querySelector('.score').textContent = score;
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too low!'
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game!'
+            document.querySelector('.score').textContent = 0;
+        }
     }
 });
