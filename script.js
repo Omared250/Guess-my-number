1c41ae75374233e9de7b057799405703a29aa7b1
Omar <omared250@gmail.com>
2021-11-24 15:33:16 +0100

    setting the highscore

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23; */
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1
-
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -27,6 +27,12 @@ document.querySelector('.check').addEventListener('click', function() {
         document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
+
+        // HighSCore values
+        if(score > highScore) {
+            highScore = score
+            document.querySelector('.highscore').textContent = highScore;
+        }
 
     // When guess is too high
     } else if (guess > secretNumber) {
